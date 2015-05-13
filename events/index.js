6f2e05baaed34379814bddd57b9b7edc25ec71a6
Will Holcomb <will@dhappy.org>
2015-05-13 16:06:37 -0400

    loading areas

--- events/index.js
+++ events/index.js
@@ -1,10 +1,20 @@
 $( function() {
-    console.log( $($('#map').get( 0 ).contentDocument).find( '#lower-patio' ) )
+    var $areas = $('<ul/>')
+    $('#menu').append( $areas )
+    var map = document.getElementById('map')
+    map.addEventListener( 'load', function() {
+	$('[class="area"]', map.contentDocument).each( function() {
+	    $areas.append( $('<li/>').text( $(this).attr( 'id' ) ) )
+	} )
+    }, false )
 
     var slideout = new Slideout( {
 	panel: document.getElementById( 'panel' ),
 	menu: document.getElementById( 'menu' ),
 	padding: 256,
 	tolerance: 70
+    } )
+    $('.toggle-button').click( function() {
+	slideout.toggle()
     } )
 } )
